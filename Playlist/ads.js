5d1fef1a4951c23b265110e40b0a1e211e1040a7
venkatraju <venkat@waspmobile.com>
2018-08-03 16:26:04 +0530

    ad tag is updated with google test double click ad tag

--- Playlist/ads.js
+++ Playlist/ads.js
@@ -22,8 +22,11 @@ var vjsOptions = {
 player = videojs('content_video', vjsOptions);
 var imaOptions = {
         id: 'content_video',
-        //adTagUrl: 'https://googleads.g.doubleclick.net/pagead/ads?client=ca-video-pub-6804475519558550&slotname=3729725924&ad_type=video&description_url=https%3A%2F%2Fconsequenceofsound.net&max_ad_duration=30000&videoad_start_delay=0&vpmute=1&vpa=1'
-		adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=1280x720|400x300|640x480&iu=/134312942/COS_Video_Test&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]'
+        adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
+        'iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&' +
+        'impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&' +
+        'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js' +
+        '&cmsid=496&vid=short_onecue&correlator='
 };
 player.ima(imaOptions);
 var adsRequest = new google.ima.AdsRequest();
--- autoplay/ads.js
+++ autoplay/ads.js
@@ -52,15 +52,11 @@ function initPlayer() {
 
   var imaOptions = {
     id: 'content_video',
-    /*adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
+    adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
         'iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&' +
         'impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&' +
         'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js' +
         '&cmsid=496&vid=short_onecue&correlator='
-	  */
-	  adTagUrl: 'https://googleads.g.doubleclick.net/pagead/ads?client=ca-video-pub-6804475519558550&slotname=3729725924&ad_type=video&description_url=https%3A%2F%2Fconsequenceofsound.net&max_ad_duration=30000&videoad_start_delay=0&vpmute=1&vpa=1'
-			 
-
   };
   player.ima(imaOptions);
 
